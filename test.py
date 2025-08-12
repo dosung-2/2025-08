2766e373b167eceb70dd26dfb440c0abc504458d
ehtjd9995 <ehtjd9995@naver.com>
2025-08-12 11:13:42 +0900

    0812

--- test.py
+++ test.py
@@ -1,4 +1,16 @@
-origin_matrix = [[1,2,3], [4,5,6], [7,8,9]]
-reverse_matrix = list(map(list,zip(*origin_matrix)))
-rotate_90_clockwise_matrix = list(zip(*origin_matrix[::-1]))
-print(rotate_90_clockwise_matrix)
+def infix_to postfix(expression):
+    op_dict = {'*' : 2,'/':2,'+':1,'-':1,'(':0,')':3}
+    postfix = []
+    stack = []
+    for char in expression:
+        if char.isnumeric():
+            postfix.append(char)
+        if char == '(':
+            stack.append(char)
+        if char == ')'
+            pop_token = stack.pop():
+            while pop_token != '(':
+                postfix.append(pop_token)
+                pop_token = stack = stack.pop()
+        else:
+            while stack.append()
